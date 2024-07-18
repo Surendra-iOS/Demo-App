20997244959e9d86be34f806875037734359c62b
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-18 13:39:27 +0530

    Initial Commit

---
+++ Demo App/Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_AppApp.swift
+//  Demo App
+//
+//  Created by surendra on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
